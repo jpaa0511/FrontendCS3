8267a196791892550dd333e5d99a06667452d63b
jpaa0511 <110578643+jpaa0511@users.noreply.github.com>
2022-11-02 01:57:25 -0500

    modal agregar plataforma, conexion a api ok

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-$(document).ready(() => {
+$(document).ready(() => {});
+
+function cargarTabla() {
+  $('main').load('../index.html');
+
+};
+
   $.get("http://localhost:3000/plataformas", (rs) => {
     let t = $("#plataformas");
     rs.forEach((Plataforma) => {
       t.append(`
         <div class="col-md-3">
-          <div class="card text-center">
+          <div class="card text-center mb-3">
             <div id="irPlataforma" class="card-body">
               <img src="${Plataforma.Imagen_URL}" alt="" width="90%" height="200px">
               <h3 class="card-title text-uppercase"> ${Plataforma.Nombre}</h3>
@@ -18,5 +24,25 @@ $(document).ready(() => {
           </div>           
         </div>`);
     });
+  });
+
+
+$("#addPlataforma").click(() => {
+  const Imagen_URL = $("#Imagen_URL");
+  const Nombre = $("#Nombre");
+  const Descripcion = $("#Descripcion");
+  const Precio = $("#Precio");
+
+  let p = {
+    Imagen_URL: Imagen_URL.val(),
+    Nombre: Nombre.val(),
+    Descripcion: Descripcion.val(),
+    Precio: Precio.val(),
+  };
+  console.log(p);
+
+  $.post("http://localhost:3000/plataformas", p, (rs) => {
+    console.log(rs);
+    cargarTabla();
   });
 });
